5511c9ebeee688bcb2fb91d5039523757b55cb1c
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-02-11 23:31:04 +1100

    Started work on serialization tests.

---
+++ JobSearch.Serialization.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace JobSearch.Serialization.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
